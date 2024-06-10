12d4ae950669b0faa85dbd677c57393568346fdb
abrarCSE29 <96782774+abrarCSE29@users.noreply.github.com>
2024-06-11 01:44:36 +0600

    added cart summary

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -12,10 +12,17 @@ export default function Cart(props) {
     else if (totalPrice > 0) {
         shippingCost = 13;
     }
+
+    const vat = (totalPrice*15.00/100).toFixed(2);
+    
+
+    const grandTotal = (totalPrice+Number.parseFloat(vat)+shippingCost).toFixed(2);
     return (
         <div className='cart'>
-            <h4>Order Summary</h4>
-            <p>Items Ordered : {props.cart.length}</p>
+            <div className='cart-heading'>
+                <h2>Order Summary</h2>
+                <p className='text-bold'>Items Ordered : {props.cart.length}</p>
+            </div>
             <ol>
                 {
                     props.cart.map((product, index) =>
@@ -25,13 +32,30 @@ export default function Cart(props) {
                                     {product.name}
                                 </div>
                                 <div className='price'>
-                                    {product.price}
+                                    ${product.price}
                                 </div>
                             </div>
-
                         </li>)
                 }
             </ol>
+            <hr />
+            <div className='cart-product'>
+                <div className='name'>Total Price</div>
+                <div className='price'>${totalPrice}</div>
+            </div>
+            <div className='cart-product'>
+                <div className='name'>Shipping Cost</div>
+                <div className='price'>${shippingCost}</div>
+            </div>
+            <div className='cart-product'>
+                <div className='name'>VAT & Tax</div>
+                <div className='price'>${vat}</div>
+            </div>
+            <hr className='hr-line' />
+            <div className='cart-product text-bold'>
+                <div className='name'>Grand Total</div>
+                <div className='price'>${grandTotal}</div>
+            </div>
 
         </div>
     )
